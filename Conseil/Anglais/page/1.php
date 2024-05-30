21c02b9d3f6eaf650f6181eb08ad4f07e959bb20
AirSoks <131533464+AirSoks@users.noreply.github.com>
2024-05-30 11:23:09 +0200

    Update 1.php

--- Conseil/Anglais/page/1.php
+++ Conseil/Anglais/page/1.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ÉduConseils - Mathématique</title>
+    <title>ÉduConseils - Anglais</title>
     <link rel="stylesheet" href="../../../style.css">
 </head>
 
